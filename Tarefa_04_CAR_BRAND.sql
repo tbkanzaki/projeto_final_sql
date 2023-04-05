60dc398c2d0bf2fd7b4d2d52fbe2824ad2b575cf
TEREZA KANZAKI <tkanzaki@gmail.com>
2023-04-05 12:43:20 -0300

    Tarefa 04 Criando e Inserido dados na tabela CAR_BRAND

---
+++ Tarefa_04_CAR_BRAND.sql
@@ -0,0 +1,22 @@
+-- SQLite
+--4 - Construa uma query SQL para criar a tabela CAR_BRAND (marcas) no banco de dados, 
+--e logo em seguida insira as marcas abaixo:
+
+CREATE TABLE CAR_BRAND (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    BRAND_NAME VARCHAR(100) NOT NULL
+);
+
+INSERT INTO CAR_BRAND (BRAND_NAME)
+VALUES 
+('Chevrolet'),
+('Toyota'),
+('Hyundai'),
+('Volkswagen'),
+('Jeep'),
+('Renault'),
+('Honda'),
+('Fiat');
+
+
+SELECT * FROM CAR_BRAND;
