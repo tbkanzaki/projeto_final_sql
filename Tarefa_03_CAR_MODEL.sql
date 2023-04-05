7cae05eb0cc062ddb4f06ad4496b0156449e2f0b
TEREZA KANZAKI <tkanzaki@gmail.com>
2023-04-05 12:39:36 -0300

    Tarefa 03 Criando e Inserido dados na tabela CAR_MODEL

---
+++ Tarefa_03_CAR_MODEL.sql
@@ -0,0 +1,23 @@
+-- SQLite
+--3 - Construa uma query SQL para criar a tabela CAR_MODEL (modelos) no banco de dados, 
+--e logo em seguida insira os modelos abaixo:
+CREATE TABLE CAR_MODEL (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    MODEL_NAME VARCHAR(100) NOT NULL
+);
+
+INSERT INTO CAR_MODEL (MODEL_NAME)
+VALUES 
+('Conversível'),
+('Sedã'),
+('Hatch'),
+('Coupé'),
+('Perua'),
+('SUV'),
+('Picape'),
+('Minivan'),
+('Utilitário'),
+('Buggy');
+
+
+SELECT * FROM CAR_MODEL;
