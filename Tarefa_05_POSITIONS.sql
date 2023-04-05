fa98c4ffa1183847e6b86b0d52c33d7d3ebc88ef
TEREZA KANZAKI <tkanzaki@gmail.com>
2023-04-05 12:46:19 -0300

    Tarefa 05 Criando e Inserido dados na tabela POSITIONS

---
+++ Tarefa_05_POSITIONS.sql
@@ -0,0 +1,18 @@
+-- SQLite
+--5 - Construa uma query SQL para criar a tabela POSITIONS (Cargos) no banco de dados, 
+--e logo em seguida insira os cargos abaixo:
+
+CREATE TABLE POSITIONS (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    DESCRIPTION VARCHAR(200) NOT NULL
+);
+
+INSERT INTO POSITIONS (DESCRIPTION)
+VALUES 
+('Gerente de vendas'),
+('Gerente de compras'),
+('Vendedor'),
+('Mecânico'),
+('Assistente Administrativo');
+
+SELECT * FROM POSITIONS;
